26efe3d219ee1b3535391227671c93be5c187c31
prashant_sonawane <p2352002@gmail.com>
2025-06-02 23:34:29 +0530

    invoiceForm - add, update, remove functions added, formData object defined

--- src/components/InvoiceForm.jsx
+++ src/components/InvoiceForm.jsx
@@ -1,16 +1,66 @@
 import { Plus, Trash2, X } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleForm } from "../store/InvoiceSlice";
+import { addDays, format, formatDate } from "date-fns";
 
 function InvoiceForm() {
   const dispatch = useDispatch();
+
+  const [formData, setFormData] = useState(() => {
+    return {
+      id: `INV${Math.floor(Math.random() * 10000)}`,
+      status: "pending",
+      billFrom: {
+        streetAddress: "",
+        city: "",
+        postCode: "",
+        country: "",
+      },
+      billTo: {
+        clientEmail: "",
+        streetAddress: "",
+        city: "",
+        postCode: "",
+        country: "",
+      },
+      clientName: "",
+      items: [],
+      paymentTerms: "Net 30 Days",
+      projectDescription: "",
+      invoiceDate: format(new Date(), "yyyy-MM-dd"),
+      dueDate: format(addDays(new Date(), 30), "yyyy-MM-dd"),
+      amount: 0,
+    };
+  });
+
+  // console.log("form data", formData)
+
+  const addItem = () => {
+    setFormData({...formData,items: [...formData.items, {name: "", quantity : 0, price: 0, total: 0}]})
+  }
+
+  const removeItem = (index) => {
+    setFormData({...formData, items : formData.items.filter((_,i)=>i !== index)})
+  }
+
+  const updateItem = (index, field, value) => {
+    const newItems = [...formData.items]
+    newItems[index][field] = value
+    if(field === "quantity" || field === "price"){
+      const qty = field === "quantity" ? value: newItems[index].quantity;
+      const price = field === "price" ? value: newItems[index].price;
+      newItems[index].total = qty*price;
+    }
+    setFormData({...formData, items: newItems})
+  }
+
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center overflow-y-auto px-4 py-8">
       <div className="bg-slate-800 p-8 rounded-lg w-full max-w-2xl my-auto">
         <div className="flex justify-between items-center mb-6">
           <h2 className="text-2xl font-bold">New Invoice</h2>
-          <button type="button" onClick={()=> dispatch(toggleForm())}>
+          <button type="button" onClick={() => dispatch(toggleForm())}>
             <X size={24} />
           </button>
         </div>
@@ -22,6 +72,16 @@ function InvoiceForm() {
             <input
               type="text"
               placeholder="Street address"
+              value={formData.billFrom.streetAddress}
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  billFrom: {
+                    ...formData.billFrom,
+                    streetAddress: e.target.value,
+                  },
+                });
+              }}
               required
               className="w-full bg-slate-900 rounded-lg p-3 text-white placeholder-gray-400"
             />
@@ -32,18 +92,48 @@ function InvoiceForm() {
               type="text"
               placeholder="City"
               required
+              value={formData.billFrom.city}
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  billFrom: {
+                    ...formData.billFrom,
+                    city: e.target.value,
+                  },
+                });
+              }}
               className="w-full bg-slate-900 rounded-lg p-3 text-white placeholder-gray-400"
             />
             <input
               type="text"
               placeholder="Post Code"
               required
+              value={formData.billFrom.postCode}
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  billFrom: {
+                    ...formData.billFrom,
+                    postCode: e.target.value,
+                  },
+                });
+              }}
               className="w-full bg-slate-900 rounded-lg p-3 text-white placeholder-gray-400"
             />
             <input
               type="text"
               placeholder="Country"
               required
+              value={formData.billFrom.country}
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  billFrom: {
+                    ...formData.billFrom,
+                    country: e.target.value,
+                  },
+                });
+              }}
               className="w-full bg-slate-900 rounded-lg p-3 text-white placeholder-gray-400"
             />
           </div>
@@ -55,19 +145,94 @@ function InvoiceForm() {
               type="text"
               placeholder="Name"
               required
+              value={formData.clientName}
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  clientName: e.target.value,
+                });
+              }}
               className="w-full bg-slate-900 rounded-lg p-3 text-white placeholder-gray-400"
             />
             <input
               type="email"
               placeholder="Client Email"
               required
+              value={formData.billTo.clientEmail}
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  billTo: {
+                    ...formData.billTo,
+                    clientEmail: e.target.value,
+                  },
+                });
+              }}
               className="w-full bg-slate-900 rounded-lg p-3 text-white placeholder-gray-400"
             />
             <input
               type="text"
               placeholder="Street address"
               required
+              value={formData.billTo.streetAddress}
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  billTo: {
+                    ...formData.billTo,
+                    streetAddress: e.target.value,
+                  },
+                });
+              }}
               className="w-full bg-slate-900 rounded-lg p-3 text-white placeholder-gray-400"
+            />
+            <input
+              type="text"
+              placeholder="City"
+              required
+              value={formData.billTo.city}
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  billTo: {
+                    ...formData.billTo,
+                    city: e.target.value,
+                  },
+                });
+              }}
+              className="w-full bg-slate-900 rounded-lg p-3 "
+            />
+            <input
+              type="text"
+              placeholder="post code"
+              required
+              value={formData.billTo.postCode}
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  billTo: {
+                    ...formData.billTo,
+                    postCode: e.target.value,
+                  },
+                });
+              }}
+              className="w-full bg-slate-900 rounded-lg p-3 "
+            />
+            <input
+              type="text"
+              placeholder="country"
+              required
+              value={formData.billTo.country}
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  billTo: {
+                    ...formData.billTo,
+                    country: e.target.value,
+                  },
+                });
+              }}
+              className="w-full bg-slate-900 rounded-lg p-3 "
             />
           </div>
 
@@ -77,10 +242,26 @@ function InvoiceForm() {
               <input
                 type="date"
                 className="bg-slate-900 rounded-lg p-3 text-white"
+                value={formData.invoiceDate}
+                onChange={(e) => {
+                  const newDate = e.target.value;
+                  setFormData({
+                    ...formData,
+                    invoiceDate: newDate,
+                    dueDate: format(
+                      addDays(new Date(newDate), 30),
+                      "yyyy-MM-dd"
+                    ),
+                  });
+                }}
               />
               <select
                 className="bg-slate-900 rounded-lg p-3 text-white"
                 required
+                value={formData.paymentTerms}
+                onChange={(e)=> {
+                  setFormData({...formData, paymentTerms: e.target.value })
+                }}
               >
                 <option>Net 30 Days</option>
                 <option>Net 60 Days</option>
@@ -90,6 +271,13 @@ function InvoiceForm() {
               type="text"
               placeholder="Project description"
               required
+              value={formData.projectDescription}
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  projectDescription: e.target.value,
+                });
+              }}
               className="w-full bg-slate-900 rounded-lg p-3 text-white"
             />
           </div>
@@ -97,7 +285,7 @@ function InvoiceForm() {
           {/* Item List */}
           <div className="space-y-4">
             <h3>Item List</h3>
-            <div className="grid grid-cols-12 gap-4 items-center">
+            {/* <div className="grid grid-cols-12 gap-4 items-center">
               <input
                 type="text"
                 placeholder="Item Name"
@@ -125,12 +313,58 @@ function InvoiceForm() {
               >
                 <Trash2 size={20} className="text-white hover:text-red-500" />
               </button>
-            </div>
+            </div> */}
+            {formData.items.map((item, index) => (
+               <div className="grid grid-cols-12 gap-4 items-center" key={index}>
+              <input
+                type="text"
+                placeholder="Item Name"
+                className="bg-slate-900 rounded-lg p-3 col-span-5 text-white"
+                value={item.name}
+                onChange={(e) => {
+                  updateItem(index,'name',e.target.value)
+                }}
+              />
+              <input
+                type="number"
+                placeholder="Quantity"
+                className="bg-slate-900 rounded-lg p-3 col-span-2 text-white"
+                min="1"
+                required
+                value={item.quantity}
+                onChange={(e) => {
+                  updateItem(index,'quantity',parseInt(e.target.value) )
+                }}
+                
+              />
+              <input
+                type="number"
+                placeholder="Price"
+                className="bg-slate-900 rounded-lg p-3 col-span-2 text-white"
+                min="0"
+                step="0.01"
+                required
+                value={item.price}
+                onChange={(e) => {
+                  updateItem(index,'price',parseFloat(e.target.value))
+                }}
+              />
+              <div className="col-span-2  text-right ">${item.total.toFixed(2)}</div>
+              <button
+                type="button"
+                className="text-slate-400 hover:text-red-500"
+                onClick={()=> removeItem(index)}
+              >
+                <Trash2 size={20} className="text-white hover:text-red-500" />
+              </button>
+            </div> 
+            ))}
 
             <button
               type="button"
               className="w-full bg-slate-700 hover:bg-slate-600 rounded-lg p-3 flex 
               items-center justify-center space-x-2 text-white"
+              onClick={addItem}
             >
               <Plus size={20} />
               <span>Add New Item</span>
